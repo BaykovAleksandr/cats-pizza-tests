233d9b677cd2ea3624a49aed62a2643719d4c1f4
Baykov Aleksandr <baykov-1988@mail.ru>
2026-04-11 12:07:51 +0300

    add webserver and home page tests

---
+++ tests/POM/specs/home.spec.ts
@@ -0,0 +1,26 @@
+import { test } from "../../fixtures/app.fixture";
+
+test.describe("Catalog and Cart (Guest)", () => {
+  test.beforeEach(async ({ homePage }) => {
+    await homePage.open();
+  });
+
+  test("Catalog opens and shows cards", async ({ homePage }) => {
+    await homePage.assertLoaded();
+    await homePage.assertCartVisible();
+  });
+
+  test("Guest adds first cat to cart and sees badge count", async ({
+    homePage,
+  }) => {
+    await homePage.addFirstCatToCart();
+    await homePage.assertCartBadgeCount(1);
+  });
+
+  test("Guest opens cart and navigates to cart page", async ({ homePage }) => {
+    await homePage.addFirstCatToCart();
+    await homePage.openCart();
+    await homePage.goToCartPage();
+    await homePage.assertCartPageOpened();
+  });
+});
